fix loss computation; change images normalization for face-vgg2

project_code/data_tools/data_util.py
@@ -2,6 +2,7 @@ import numpy as np
 import scipy.io as sio
 import tensorflow as tf
 
+from data_tools.data_const import face_vgg2_input_mean
 from morphable_model.model.morphable_model import FFTfMorphableModel
 
 
@@ -19,8 +20,9 @@ def load_image_3dmm(image_file: str):
 
     # resize image to (224, 224)
     image = tf.image.resize(image, [224, 224])
-    # normalize image to [-1, 1]
-    image = (image / 127.5) - 1
+    # # normalize image to [-1, 1]
+    # image = (image / 127.5) - 1
+    image -= face_vgg2_input_mean
     return image
 
 

project_code/models/networks_linear_3dmm.py
@@ -102,8 +102,8 @@ class FaceNetLinear3DMM:
             model_type='BFM'
         )
 
-    def __call__(self, inputs, training=True):
-        output = self.model(inputs)
+    def __call__(self, inputs, training=False):
+        output = self.model(inputs, training=training)
         x_illum = tf.expand_dims(output[:, 0: self.output_size_structure[0]], axis=1)
         x_color = tf.expand_dims(output[:, self.output_size_structure[0]: self.output_size_structure[1]], axis=1)
         x_tex = tf.expand_dims(output[:, self.output_size_structure[1]: self.output_size_structure[2]], axis=2)
@@ -161,6 +161,13 @@ class FaceNetLinear3DMM:
         self._train_3dmm()
 
     def _train_3dmm(self):
+        self.resnet50.unfreeze()
+        # we load face_vgg2 for computing loss for unsupervised learning
+        # face_vgg2 will not be trainable
+        self.face_vgg2 = Resnet(image_size=self.image_size)
+        self.face_vgg2.build()
+        self.face_vgg2.freeze()
+
         ckpt = tf.train.Checkpoint(step=tf.Variable(0), net=self.model)
         checkpoint_dir = self.model_dir
         manager = tf.train.CheckpointManager(ckpt, checkpoint_dir, max_to_keep=self.config_train.max_checkpoint_to_keep)

project_code/training/loss.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 
+from data_tools.data_const import face_vgg2_input_mean
+
 
 def loss_norm(est, gt, loss_type):
     """
@@ -37,12 +39,16 @@ def loss_3dmm_warmup(gt: dict, est: dict, metric: dict, loss_types: dict, loss_w
     return G_loss
 
 
-def loss_3dmm(images, images_rendered, metric, loss_type):
-    # recover original images
-    images = (images + 1) * 127.5
-    images_masked = tf.where(tf.greater(images_rendered, 0), images, images_rendered)
-    G_loss = loss_norm(est=images_rendered, gt=images_masked, loss_type=loss_type) + \
-        0.3 * loss_norm(est=images_rendered, gt=images, loss_type=loss_type)
+def loss_3dmm(face_vgg2, images, images_rendered, metric, loss_type):
+    # original images
+    gt = face_vgg2(images, training=False)
+
+    # rendered images
+    images_rendered -= face_vgg2_input_mean
+    est = face_vgg2(images_rendered, training=False)
+
+    # compute loss
+    G_loss = loss_norm(est=est, gt=gt, loss_type=loss_type) + 0.3 * loss_norm(est=images_rendered, gt=images, loss_type=loss_type)
 
     metric.update_state(G_loss)
     return G_loss