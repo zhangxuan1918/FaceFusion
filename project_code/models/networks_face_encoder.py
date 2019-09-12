983df536e0743e5eb2780a14b7c8a1b8a9fc6ead
zhangxuan1918 <zhangxuan1918@gmail.com>
2019-09-12 21:00:58 +0200

    update networks_face_encoder output size for different params

--- project_code/models/networks_face_encoder.py
+++ project_code/models/networks_face_encoder.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 
 
 class FaceEncoder:
-    def __init__(self, image_size, gf_dim, gfc_dim, sh_dim, tx_dim, co_dim, m_dim, il_dim):
+    def __init__(self, image_size, gf_dim, gfc_dim, sh_dim, tx_dim, co_dim, m_dim, il_dim, ep_dim):
         self.image_size = image_size
         self.bn_axis = 3
         self.gf_dim = gf_dim
@@ -15,6 +15,7 @@ class FaceEncoder:
         self.sh_dim = sh_dim
         self.tx_dim = tx_dim
         self.co_dim = co_dim
+        self.ep_dim = ep_dim
 
     def build(self):
         inputs = Input(shape=[self.image_size, self.image_size, 3], name='image_input')
@@ -76,44 +77,41 @@ class FaceEncoder:
         k4_1 = elu(self.g_bn4_1(Conv2D(self.gf_dim * 5, (3, 3), (1, 1), padding='SAME', use_bias=False, name='g_k41_conv')(k4_0), training=is_training))
 
         # Pose
-        k51_m = self.g_bn5_m(Conv2D(int(self.gfc_dim / 5), (3, 3), (1, 1), padding='SAME', use_bias=False, name='g_k5_m_conv')(k4_1), training=is_training)
+        k51_m = self.g_bn5_m(Conv2D(int(self.gfc_dim / 8), (3, 3), (1, 1), padding='SAME', use_bias=False, name='g_k5_m_conv')(k4_1), training=is_training)
         k51_shape_ = k51_m.shape
         k52_m = AveragePooling2D(pool_size=[k51_shape_[1], k51_shape_[2]], strides=[1, 1], padding='VALID')(k51_m)
         k52_m = tf.reshape(k52_m, [-1, int(self.gfc_dim / 5)])
         k6_m = Dense(self.m_dim, name='g_k6_m_lin')(k52_m)
 
         # Illumination
-        k51_ill = self.g_bn5_ill(Conv2D(int(self.gfc_dim / 5), (3, 3), (1, 1), padding='SAME', name='g_k5_il_conv')(k4_1), training=is_training)
+        k51_ill = self.g_bn5_ill(Conv2D(int(self.gfc_dim / 8), (3, 3), (1, 1), padding='SAME', name='g_k5_il_conv')(k4_1), training=is_training)
         k52_ill = AveragePooling2D(pool_size=[k51_shape_[1], k51_shape_[2]], strides=[1, 1], padding='VALID')(k51_ill)
         k52_ill = tf.reshape(k52_ill, [-1, int(self.gfc_dim / 5)])
         k6_ill = Dense(self.il_dim, name='g_k6_ill_lin')(k52_ill)
 
         # Shape
-        k51_shape = self.g_bn5_shape(Conv2D(int(self.gfc_dim / 2), (3, 3), (1, 1), padding='SAME', name='g_k5_shape_conv')(k4_1), training=is_training)
+        k51_shape = self.g_bn5_shape(Conv2D(self.sh_dim, (3, 3), (1, 1), padding='SAME', name='g_k5_shape_conv')(k4_1), training=is_training)
         k52_shape = AveragePooling2D(pool_size=[k51_shape_[1], k51_shape_[2]], strides=[1, 1], padding='VALID')(k51_shape)
-        k52_shape = tf.reshape(k52_shape, [-1, int(self.gfc_dim / 2)])
-        k6_shape = Dense(self.sh_dim, name='g_k6_sh_lin')(k52_shape)
+        k52_shape = tf.reshape(k52_shape, [-1, self.sh_dim])
 
         # Texture
-        k51_tex = self.g_bn5_tex(Conv2D(int(self.gfc_dim / 2), (3, 3), (1, 1), padding='SAME', name='g_k5_tex_conv')(k4_1), training=is_training)
+        k51_tex = self.g_bn5_tex(Conv2D(self.tx_dim, (3, 3), (1, 1), padding='SAME', name='g_k5_tex_conv')(k4_1), training=is_training)
         k52_tex = AveragePooling2D(pool_size=[k51_shape_[1], k51_shape_[2]], strides=[1, 1], padding='VALID')(
             k51_tex)
-        k52_tex = tf.reshape(k52_tex, [-1, int(self.gfc_dim / 2)])
-        k6_tex = Dense(self.tx_dim, name='g_k6_tex_lin')(k52_tex)
+        k52_tex = tf.reshape(k52_tex, [-1, self.tx_dim])
 
         # Expression
-        k51_exp = self.g_bn5_exp(Conv2D(int(self.gfc_dim / 5), (3, 3), (1, 1), padding='SAME', name='g_k5_exp_conv')(k4_1), training=is_training)
+        k51_exp = self.g_bn5_exp(Conv2D(self.ep_dim, (3, 3), (1, 1), padding='SAME', name='g_k5_exp_conv')(k4_1), training=is_training)
         k52_exp = AveragePooling2D(pool_size=[k51_shape_[1], k51_shape_[2]], strides=[1, 1], padding='VALID')(k51_exp)
-        k52_exp = tf.reshape(k52_exp, [-1, int(self.gfc_dim / 2)])
-        k6_exp = Dense(self.sh_dim, name='g_k6_exp_lin')(k52_exp)
+        k52_exp = tf.reshape(k52_exp, [-1, self.ep_dim])
 
         # Color
-        k51_col = self.g_bn5_col(Conv2D(int(self.gfc_dim / 5), (3, 3), (1, 1), padding='SAME', name='g_k5_col_conv')(k4_1), training=is_training)
+        k51_col = self.g_bn5_col(Conv2D(int(self.gfc_dim / 8), (3, 3), (1, 1), padding='SAME', name='g_k5_col_conv')(k4_1), training=is_training)
         k52_col = AveragePooling2D(pool_size=[k51_shape_[1], k51_shape_[2]], strides=[1, 1], padding='VALID')(k51_col)
         k52_col = tf.reshape(k52_col, [-1, int(self.gfc_dim / 2)])
         k6_col = Dense(self.co_dim, name='g_k6_col_lin')(k52_col)
 
-        return k6_shape, k6_tex, k6_exp, k6_m, k6_ill, k6_col
+        return k52_shape, k52_tex, k52_exp, k6_m, k6_ill, k6_col
 
 
 if __name__ == '__main__':
@@ -121,8 +119,9 @@ if __name__ == '__main__':
         image_size=224,
         gf_dim=32,
         gfc_dim=512,
-        sh_dim=80,
-        tx_dim=80,
+        sh_dim=199,
+        ep_dim=29,
+        tx_dim=40,
         co_dim=7,
         m_dim=7,
         il_dim=10
