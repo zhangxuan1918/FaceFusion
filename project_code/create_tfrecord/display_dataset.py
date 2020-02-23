fix bugs in generating tfrecord; bug in normalizing pose parameter, image size is wrong

project_code/create_tfrecord/display_dataset.py
@@ -1,50 +1,62 @@
 import tensorflow as tf
-
-from create_tfrecord.export_tfrecord_util import split_300W_LP_labels
+from tf_3dmm.morphable_model.morphable_model import TfMorphableModel
+from create_tfrecord.export_tfrecord_util import split_300W_LP_labels, fn_unnormalize_300W_LP_labels
 from training import dataset
+from tf_3dmm.mesh.render import render_2
+import numpy as np
 
 
-def display(tfrecord_dir):
+def display(tfrecord_dir, bfm_path, image_size):
     print('Loading dataset %s' % tfrecord_dir)
-    # gpus = tf.config.experimental.list_physical_devices('GPU')
-    # if gpus:
-    #     try:
-    #         # Currently, memory growth needs to be the same across GPUs
-    #         for gpu in gpus:
-    #             tf.config.experimental.set_memory_growth(gpu, True)
-    #         logical_gpus = tf.config.experimental.list_logical_devices('GPU')
-    #         print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
-    #     except RuntimeError as e:
-    #         # Memory growth must be set before GPUs have been initialized
-    #         print(e)
 
     dset = dataset.TFRecordDataset(
         tfrecord_dir, batch_size=1, max_label_size='full', repeat=False, shuffle_mb=0)
+
+    print('Loading BFM model')
+    bfm = TfMorphableModel(
+        model_path=bfm_path,
+        model_type='BFM'
+    )
+
     import cv2
 
     idx = 0
-    while True:
+    filename = '/opt/project/output/verify_dataset/20200222/image_{0}.jpg'
+    fn_unnormalize_labels = fn_unnormalize_300W_LP_labels(bfm_path=bfm_path, image_size=image_size)
+    while idx < 5:
         try:
             image_tensor, labels_tensor = dset.get_minibatch_tf()
             image = image_tensor[0].numpy()
         except tf.errors.OutOfRangeError:
             break
 
-        # TODO: render images using labels
-        # roi, lm, pp, shape_para, exp_para, color_para, illum_para, tex_para = split_300W_LP_labels(labels_tensor)
-        if idx == 0:
-            print('Displaying images')
-            cv2.namedWindow('dataset_tool')
-            print('Press SPACE or ENTER to advance, ESC to exit')
-        print('\nidx = %-8d' % idx)
-        cv2.imshow('dataset_tool', image[:, :, ::-1])
+        # render images using labels
+        roi, landmarks, pose_para, shape_para, exp_para, color_para, illum_para, tex_para = split_300W_LP_labels(labels_tensor)
+        roi, landmarks, pose_para, shape_para, exp_para, color_para, illum_para, tex_para = fn_unnormalize_labels(roi, landmarks, pose_para, shape_para, exp_para, color_para, illum_para, tex_para)
+        image_rendered = render_2(
+            angles_grad=pose_para[0, 0, 0:3],
+            t3d=pose_para[0, 0, 3:6],
+            scaling=pose_para[0, 0, 6],
+            shape_param=shape_para[0],
+            exp_param=exp_para[0],
+            tex_param=tex_para[0],
+            color_param=color_para[0],
+            illum_param=illum_para[0],
+            frame_height=224,
+            frame_width=224,
+            tf_bfm=bfm
+        ).numpy()
+
+        images = np.concatenate((image, image_rendered), axis=0)
+        cv2.imwrite(filename.format(idx), images)
         idx += 1
-        if cv2.waitKey() == 27:
-            break
 
     print('\nDisplayed %d images' % idx)
 
 
 if __name__ == '__main__':
     tfrecord_dir = '/opt/data/face-fuse/test/'
-    display(tfrecord_dir)
+    bfm_path = '/opt/data/BFM/BFM.mat'
+    image_size = 224
+
+    display(tfrecord_dir, bfm_path, image_size)

project_code/create_tfrecord/export_tfrecord.py
@@ -41,9 +41,10 @@ class TFRecordExporter:
             print('%-40s\r' % '', end='', flush=True)
             print('Add %d images.' % self.cur_images)
 
-    def choose_shuffled_order(self):
+    def choose_shuffled_order(self, random_shuffle):
         order = np.arange(self.expected_images)
-        np.random.RandomState(123).shuffle(order)
+        if random_shuffle:
+            np.random.RandomState(123).shuffle(order)
         return order
 
     def add_image(self, img):
@@ -84,12 +85,12 @@ class TFRecordExporter:
 
 
 def create_tfrecord(tfrecord_dir, image_filenames, image_size, process_label_fn, print_progress, progress_interval,
-                    resolution=224, label_size=430):
+                    resolution=224, label_size=430, random_shuffle=True):
     with TFRecordExporter(tfrecord_dir=tfrecord_dir,
                           expected_images=len(image_filenames),
                           print_progress=print_progress,
                           progress_interval=progress_interval) as tfr:
-        order = tfr.choose_shuffled_order()
+        order = tfr.choose_shuffled_order(random_shuffle=random_shuffle)
         labels = np.zeros((len(image_filenames), label_size), np.float32)
         for idx in range(order.size):
             try:
@@ -152,10 +153,11 @@ def create_AFLW_2000(tfrecord_dir, data_dir, print_progress, progress_interval, 
     if len(image_filenames) != expected_images:
         raise Exception('Expected to find %d images in \'%s\'' % (expected_images, sub_folder))
     print('find %d images' % len(image_filenames))
-
+    # sort images
+    image_filenames.sort()
     return create_tfrecord(tfrecord_dir=tfrecord_dir, image_filenames=image_filenames, image_size=image_size,
-                           process_label_fn=fn_extract_300W_LP_labels(bfm_path=bfm_path, image_size=resolution, is_aflw_2000=True), print_progress=print_progress,
-                           progress_interval=progress_interval, resolution=resolution)
+                           process_label_fn=fn_extract_300W_LP_labels(bfm_path=bfm_path, image_size=image_size, is_aflw_2000=True), print_progress=print_progress,
+                           progress_interval=progress_interval, resolution=resolution, random_shuffle=False)
 
 
 if __name__ == '__main__':

project_code/create_tfrecord/export_tfrecord_util.py
@@ -31,8 +31,8 @@ def fn_extract_300W_LP_labels(bfm_path, image_size, is_aflw_2000=False):
 
         # update pose param due to image resizing
         pp = mat['Pose_Para']
-        pp[0, 3:6] /= image_size # translation in 3d
-        pp[0, 6] *= 1000 # scaling
+        pp[0, 3:6] /= image_size  # translation in 3d
+        pp[0, 6] *= 1000  # scaling
 
         # update landmarks
         if is_aflw_2000:
@@ -40,7 +40,7 @@ def fn_extract_300W_LP_labels(bfm_path, image_size, is_aflw_2000=False):
         else:
             lm = mat['pt2d'] * rescale
 
-        lm = np.reshape(lm, (-1, )) # to recover lm, np.reshape(lm, (2, -1))
+        lm = np.reshape(lm, (-1,))  # to recover lm, np.reshape(lm, (2, -1))
 
         # update roi
         roi = np.round(mat['roi'] * rescale)
@@ -53,12 +53,11 @@ def fn_extract_300W_LP_labels(bfm_path, image_size, is_aflw_2000=False):
         color_para = mat['Color_Para'][:, :6]
         illum_para = mat['Illum_Para'][:, :9]
         return np.concatenate((roi, lm, pp, shape_para, exp_para, color_para, illum_para, tex_para[:40, :]), axis=None)
+
     return get_labels
 
 
 def split_300W_LP_labels(labels):
-    # label file has the same name as image
-    # mat format
     # roi: shape=(1, 4)
     # Shape_Para: shape=(199, 1)
     # Pose_Para: shape=(1, 7)
@@ -84,8 +83,53 @@ def split_300W_LP_labels(labels):
     """
     assert isinstance(labels, tf.Tensor) and labels.shape[1] == 430
     roi, lm, pp, shape_para, exp_para, color_para, illum_para, tex_para = \
-        tf.split(labels, num_or_size_splits=[4, 136, 7, 199, 29, 6, 9, 40])
+        tf.split(labels, num_or_size_splits=[4, 136, 7, 199, 29, 6, 9, 40],  axis=1)
     return roi, lm, pp, shape_para, exp_para, color_para, illum_para, tex_para
+
+
+def fn_unnormalize_300W_LP_labels(bfm_path, image_size):
+    # roi: shape=(None, 1, 4)
+    # Shape_Para: shape=(None, 199, 1)
+    # Pose_Para: shape=(None, 1, 7)
+    # Exp_Para: shape=(None, 29, 1)
+    # Color_Para: shape=(None, 1, 6): remove last value as it's always 1
+    # Illum_Para: shape=(None, 1, 9): remove last value as it's always 2
+    # pt2d: shape=(None, 2, 68)
+    # Tex_Para: shape=(None, 40, 1)
+    # Total: 430 params
+
+    # Load BFM
+    content = sio.loadmat(bfm_path)
+    model = content['model']
+    model = model[0, 0]
+
+    shape_ev = model['shapeEV'].astype(np.float32)
+    tex_ev = model['texEV'].astype(np.float32)
+    image_size = 1.0 * image_size
+
+    def unnormalize_labels(roi, landmarks, pose_para, shape_para, exp_para, color_para, illum_para, tex_para):
+        batch_size = roi.shape[0]
+
+        # translation and scaling
+        pose_para_new = tf.concat([pose_para[:, 0:3], pose_para[:, 3:6] * image_size, pose_para[:, 6:] / 1000.], axis=1)
+
+        landmarks = tf.reshape(landmarks, (batch_size, 2, -1))  # (None, 2, 68)
+
+        # rescale shape, exp and tex by their eigenvalue
+        shape_para *= shape_ev
+        tex_para *= tex_ev
+
+        # Color_Para: add last value as it's always 1
+        # Illum_Para: add last value as it's always 2
+        color_para = tf.concat([color_para, tf.constant(1.0, shape=(batch_size, 1))], axis=1)
+        illum_para = tf.concat([illum_para, tf.constant(2.0, shape=(batch_size, 1))], axis=1)
+
+        return roi, landmarks, tf.expand_dims(pose_para_new, axis=1), tf.expand_dims(shape_para, axis=2), \
+               tf.expand_dims(exp_para, axis=2), tf.expand_dims(color_para, 1), tf.expand_dims(illum_para, axis=1), \
+               tf.expand_dims(tex_para, axis=2)
+
+    return unnormalize_labels
+
 
 def fn_extract_coarse_80k(img_filename):
     # label file has the same name as image