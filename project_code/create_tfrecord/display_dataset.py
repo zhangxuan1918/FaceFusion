ee5ead87a710a9b97ac5cacbf30c5eee4e2fbdb1
zhangxuan1918 <zhangxuan1918@gmail.com>
2020-03-05 20:46:25 +0100

    minor changes

--- project_code/create_tfrecord/display_dataset.py
+++ project_code/create_tfrecord/display_dataset.py
@@ -58,7 +58,20 @@ def display(tfrecord_dir, bfm_path, image_size, num_images=5, n_tex_para=40):
 
 
 if __name__ == '__main__':
-    tfrecord_dir = '/opt/data/face-fuse/test/'
+
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    if gpus:
+        try:
+            # Currently, memory growth needs to be the same across GPUs
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
+            logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+        except RuntimeError as e:
+            # Memory growth must be set before GPUs have been initialized
+            print(e)
+
+    tfrecord_dir = '/opt/data/face-fuse/train/'
     bfm_path = '/opt/data/BFM/BFM.mat'
     image_size = 224
     num_images = 8
